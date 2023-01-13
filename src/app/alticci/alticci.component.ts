9501c117d5ec94fcebcf8b8a79306785eab264ae
lmmiranda <lmmiranda412@gmail.com>
2023-01-13 14:16:45 +0000

    add layout

---
+++ src/app/alticci/alticci.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-alticci',
+  templateUrl: './alticci.component.html',
+  styleUrls: ['./alticci.component.scss']
+})
+export class AlticciComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
